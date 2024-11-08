ac5069e10ba057a901612bf943f53673c6d0afe8
Diego Rockenbach <rdiegomatias@gmail.com>
2024-11-08 18:47:40 -0300

    main.c atualizado com alocação dinâmica e melhor modularização

--- main.c
+++ main.c
@@ -2,92 +2,90 @@
 #include <stdlib.h>
 #include <time.h>
 
-int* generateValues(int numValues, int biggestNum, int* values){
-  int i;
+#define NUM_BUCKETS 5
+#define BUCKET_INTERVAL 100
 
+int* generateValues(int numValues, int maxValue) {
+  
+  int* values = (int*)malloc(numValues * sizeof(int));
+  
   srand(time(NULL));
-
-  for (i = 0; i < numValues; i++) { 
-    values[i] = rand() % biggestNum;
+  
+  for (int i = 0; i < numValues; i++) {
+      values[i] = rand() % (maxValue + 1);
   }
 
   return values;
 }
 
-void printaBuckets(int numValues, int buckets[5][numValues], int index[]){
+void distributeBetweenBuckets(int* values, int numValues, int** buckets, int* bucketSizes, int maxValue) {
+  
+  int bucketIndex;
 
-  int i;
-
-  printf("\n\nBUCKET 0 (0-100):\n");
-  for (i = 0; i < index[0]; i++){
-    printf("[%d]; ", buckets[0][i]);
-  }
-  printf("\n\nBUCKET 1 (101-200):\n");
-  for (i = 0; i < index[1]; i++){
-    printf("[%d]; ", buckets[1][i]);
-  }
-  printf("\n\nBUCKET 2 (201-300):\n");
-  for (i = 0; i < index[2]; i++){
-    printf("[%d]; ", buckets[2][i]);
-  }
-  printf("\n\nBUCKET 3 (301-400):\n");
-  for (i = 0; i < index[3]; i++){
-    printf("[%d]; ", buckets[3][i]);
-  }
-  printf("\n\nBUCKET 4 (401-500):\n");
-  for (i = 0; i < index[4]; i++){
-    printf("[%d]; ", buckets[4][i]);
+  for (int i = 0; i < numValues; i++) {
+    bucketIndex = (values[i] * NUM_BUCKETS) / (maxValue + 1);
+    buckets[bucketIndex][bucketSizes[bucketIndex]] = values[i];
+    bucketSizes[bucketIndex]++;
   }
 
 }
 
-int main(){
-  int i, numValues = 1000, bucket[5][numValues], index[5] = {0, 0, 0, 0, 0};
-  int *values;
-
-  values = (int*) malloc(numValues * sizeof(int));
-  values = generateValues(numValues, 500, values);
-
-  for (i = 0; i < numValues; i++){
-    if (values[i] <= 100){
-      bucket[0][index[0]] = values[i];
-      index[0]++;
-    }
-    else if (values[i] >= 101 && values[i] <= 200){
-      bucket[1][index[1]] = values[i];
-      index[1]++;
-    }
-    else if (values[i] >= 201 && values[i] <= 300){
-      bucket[2][index[2]] = values[i];
-      index[2]++;
-    }
-    else if (values[i] >= 301 && values[i] <= 400){
-      bucket[3][index[3]] = values[i];
-      index[3]++;
-    }
-    else if (values[i] >= 401 && values[i] <= 500){
-      bucket[4][index[4]] = values[i];
-      index[4]++;
-    }
-  }
-
-  printaBuckets(numValues, bucket, index);
-
-
-  // INSERTION SORT (só no bucket 0)
-  for (i = 1; i < index[0]; i++) {
-    int key = bucket[0][i];
+void insertionSort(int *bucket, int size){
+  
+  int i;
+  
+  for (i = 1; i < size; i++) {
+    int key = bucket[i];
     int j = i - 1;
 
-    while (j >= 0 && bucket[0][j] > key) {
-        bucket[0][j + 1] = bucket[0][j];
+    while (j >= 0 && bucket[j] > key) {
+        bucket[j + 1] = bucket[j];
         j = j - 1;
     }
-    bucket[0][j + 1] = key;
+    bucket[j + 1] = key;
   }
 
+}
 
-  printaBuckets(numValues, bucket, index);
+void printaBuckets(int** buckets, int* bucketSizes){
+
+  int i, j;
+
+  for (i = 0; i < NUM_BUCKETS; i++){
+    printf("\nBUCKET %d:\n", i);
+    for (j = 0; j < bucketSizes[i]; j++){
+      printf("[%d]; ", buckets[i][j]);
+    }
+  }
+  
+}
+
+int main(){
+
+  int i, numValues = 1000, maxValue = 500, bucketSizes[NUM_BUCKETS] = {0};
+  int *values, *buckets[NUM_BUCKETS];
+
+  values = generateValues(numValues, maxValue);
+
+  for (i = 0; i < NUM_BUCKETS; i++) {
+    buckets[i] = (int*)malloc(numValues * sizeof(int));
+  }
+
+  distributeBetweenBuckets(values, numValues, buckets, bucketSizes, maxValue);
+
+  printaBuckets(buckets, bucketSizes);
+
+  for (i = 0; i < NUM_BUCKETS; i++){
+    insertionSort(buckets[i], bucketSizes[i]);
+  }
+
+  printaBuckets(buckets, bucketSizes);
+
+  // Liberar memória
+  free(values);
+  for (int i = 0; i < NUM_BUCKETS; i++) {
+    free(buckets[i]);
+  }
 
   return 0;
 }
