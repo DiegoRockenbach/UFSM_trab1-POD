04b5eaafc323e34b9f6cbb057b9c344247bbb465
Diego Rockenbach <rdiegomatias@gmail.com>
2024-11-08 19:16:27 -0300

    main.c atualizado com insertion, bubble, cocktailShaker e quick sort

--- main.c
+++ main.c
@@ -3,7 +3,6 @@
 #include <time.h>
 
 #define NUM_BUCKETS 5
-#define BUCKET_INTERVAL 100
 
 int* generateValues(int numValues, int maxValue) {
   
@@ -27,7 +26,6 @@ void distributeBetweenBuckets(int* values, int numValues, int** buckets, int* bu
     buckets[bucketIndex][bucketSizes[bucketIndex]] = values[i];
     bucketSizes[bucketIndex]++;
   }
-
 }
 
 void insertionSort(int *bucket, int size){
@@ -44,6 +42,81 @@ void insertionSort(int *bucket, int size){
     }
     bucket[j + 1] = key;
   }
+}
+
+void bubbleSort(int *bucket, int size){
+
+  int i, j, temp;
+
+  for (i = 0; i < size - 1; i++) {
+    for (j = 0; j < size - i - 1; j++) {
+      if (bucket[j] > bucket[j + 1]) {
+        temp = bucket[j];
+        bucket[j] = bucket[j + 1];
+        bucket[j + 1] = temp;
+      }
+    }
+  }
+}
+
+void cocktailShakerSort(int *bucket, int size){
+
+  int i, temp, swapped = 1, start = 0, end = size - 1;
+
+  while (swapped) {
+    swapped = 0;
+
+    for (i = start; i < end; i++) {
+      if (bucket[i] > bucket[i + 1]) {
+        temp = bucket[i];
+        bucket[i] = bucket[i + 1];
+        bucket[i + 1] = temp;
+        swapped = 1;
+      }
+    }
+
+    if (!swapped) break;
+    end--;
+
+    for (i = end - 1; i >= start; i--) {
+      if (bucket[i] > bucket[i + 1]) {
+        temp = bucket[i];
+        bucket[i] = bucket[i + 1];
+        bucket[i + 1] = temp;
+        swapped = 1;
+      }
+    }
+
+    start++;
+  }
+
+}
+
+void quickSort(int *bucket, int low, int high){
+
+  int i, temp, pivot, pi;
+
+  if (low < high) {
+    pivot = bucket[high];
+    i = low - 1;
+
+    for (int j = low; j < high; j++) {
+        if (bucket[j] < pivot) {
+            i++;
+            temp = bucket[i];
+            bucket[i] = bucket[j];
+            bucket[j] = temp;
+        }
+    }
+    temp = bucket[i + 1];
+    bucket[i + 1] = bucket[high];
+    bucket[high] = temp;
+
+    pi = i + 1;
+
+    quickSort(bucket, low, pi - 1);
+    quickSort(bucket, pi + 1, high);
+  }
 
 }
 
@@ -57,7 +130,6 @@ void printaBuckets(int** buckets, int* bucketSizes){
       printf("[%d]; ", buckets[i][j]);
     }
   }
-  
 }
 
 int main(){
@@ -75,9 +147,23 @@ int main(){
 
   printaBuckets(buckets, bucketSizes);
 
-  for (i = 0; i < NUM_BUCKETS; i++){
+  // Algoritmos de ordenação:
+
+/*   for (i = 0; i < NUM_BUCKETS; i++){
     insertionSort(buckets[i], bucketSizes[i]);
-  }
+  } */
+
+/*   for (i = 0; i < NUM_BUCKETS; i++){
+    bubbleSort(buckets[i], bucketSizes[i]);
+  } */
+
+/*   for (i = 0; i < NUM_BUCKETS; i++){
+    cocktailShakerSort(buckets[i], bucketSizes[i]);
+  } */
+
+/*   for (i = 0; i < NUM_BUCKETS; i++){
+    quickSort(buckets[i], 0, bucketSizes[i]);
+  } */
 
   printaBuckets(buckets, bucketSizes);
 
